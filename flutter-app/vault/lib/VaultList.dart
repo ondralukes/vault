945f326b2d71353715ac59d30d4502779bb5ba20
ondralukes <mail@ondralukes.cz>
2020-03-31 19:53:28 +0200

    Added vault tab bar

--- flutter-app/vault/lib/VaultList.dart
+++ flutter-app/vault/lib/VaultList.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vault/VaultMenu.dart';
 
 import 'Classes.dart';
 
@@ -29,30 +30,38 @@ class VaultListState extends State<VaultList> {
 
   buildRow(i) {
     if (i % 2 == 0) {
-      final vault = vaults[i~/2];
-      return Row(
+      final vault = vaults[i ~/ 2];
+      return InkWell(
+        onTap: (){
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => VaultMenu(
+                  //TODO: pass serverAPI
+                )
+              )
+          );
+        },
+          child: Row(
         children: <Widget>[
           Container(
             margin: EdgeInsets.all(25),
             child: Padding(
-              padding: EdgeInsets.all(10),
-                child:Icon(
-              Icons.lock,
-              size: 50,
-            )),
+                padding: EdgeInsets.all(10),
+                child: Icon(
+                  Icons.lock,
+                  size: 50,
+                )),
             decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
-              shape: BoxShape.circle
-            ),
+                color: Theme.of(context).primaryColor, shape: BoxShape.circle),
           ),
           Text(vault.codename)
         ],
-      );
+      ));
     } else
     //Ignore last divider
     if (vaults.length * 2 - 1 != i) {
-      return
-        Divider(
+      return Divider(
         color: Theme.of(context).primaryColor,
         thickness: 3,
       );
---
+++ flutter-app/vault/lib/VaultMenu.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class VaultMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: SafeArea(
+            child: DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: TabBar(
+          labelColor: Theme.of(context).primaryColor,
+          indicatorColor: Theme.of(context).primaryColor,
+          tabs: <Widget>[
+            Tab(
+              text: 'Messages',
+              icon: Icon(Icons.message),
+            ),
+            Tab(
+              text: 'Settings',
+              icon: Icon(Icons.settings),
+            )
+          ],
+        ),
+        backgroundColor: Theme.of(context).backgroundColor,
+      ),
+    )),
+    color: Theme.of(context).backgroundColor,);
+  }
+}
